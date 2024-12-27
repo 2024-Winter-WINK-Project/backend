9264a1effc044a659f842a18599fd73e5eeacb7b
kimmoongi <moongi0320@naver.com>
2024-12-28 00:06:46 +0900

    Initial Commit

---
+++ src/test/java/com/WinkProject/WinkProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WinkProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinkProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
